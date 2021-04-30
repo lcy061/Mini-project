c5e99f931cddb529dce8353578ef31b824f0beae
lcy061 <61354751+lcy061@users.noreply.github.com>
2021-04-30 21:47:41 +0900

    add Remark

--- movie.h
+++ movie.h
@@ -9,15 +9,21 @@ typedef struct {
   int ranking;
 } Movie;
 
+//Menu
 int selectMenu();
 int selectDataNo(Score s[], int count);
 
+//CRUD
 void Create(Movie *s, int count);
 void Read(Movie *s, int count);
 void Update(Movie *s, int num);
 void Delete(Movie *s, int num);
+
+//Save&Load
 void saveData(Movie *s, int count);
 int loadData(Movie *s);
+
+//Search
 void searchMovie(Movie *s, int count);
 void searchStar(Movie *s, int count);
 void searchDirector(Movie *s, int count);
